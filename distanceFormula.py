752e432655abbcae1239d3efe76060c55e6fb9fe
souravkolathur <98595356+souravkolathur@users.noreply.github.com>
2024-06-03 00:20:32 +0530

    Create distanceFormula.py

---
+++ distanceFormula.py
@@ -0,0 +1,17 @@
+x1 = int(input("Enter x-coordinate of point1 : "))
+y1 = int(input("Enter y-coordinate of point1 : "))
+x2 = int(input("Enter x-coordinate of point2 : "))
+y2 = int(input("Enter y-coordinate of point2 : "))
+distance = ((x1-x2)**2 + (y1-y2)**2)**0.5
+print("distance = ",distance)
+"""
+ouput
+......
+
+Enter x-coordinate of point1 : 2
+Enter y-coordinate of point1 : 4
+Enter x-coordinate of point2 : 4
+Enter y-coordinate of point2 : 4
+distance =  2.0
+
+"""
